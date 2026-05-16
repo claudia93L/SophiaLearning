2a34cca1c8a0bf4af4361a0a3ee4ce1c6fca177b
claudia93L <34545518+claudia93L@users.noreply.github.com>
2026-05-16 10:49:21 +0200

    cambio colore orari business e aggiunti link al file di script js e aggiunti 3 alert

---
+++ IntroToWebDev/Touchstone 3/script.js
@@ -0,0 +1,11 @@
+const showAlertSubscribe = () => {
+  alert('Thank you for subscribing to our newsletter!');
+};
+
+const showAlertMessageSent = () => {
+  alert('Thank you for contacting us! We will get back to you shortly.');
+};
+
+const showAlertClearForm = () => {
+  alert('The form has been cleared.');
+};
